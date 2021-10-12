2c62ca615a7dc26effa0ef4377d7d0c932e9a14d
FingRinger <ard21study@gmail.com>
2021-10-12 21:24:33 +0300

    Do we need separate class for some fields in Issue class?

---
+++ src/main/java/ru/netology/domain/Issue.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class Issue {
+    private int id;
+    private String author;
+    private String project;
+    private String lable;
+    private String assignee;
+    private String issueTitle;
+    private String issueBody;
+    private String attachment;
+    private boolean open;
+    private int secondsSinceOpen;
+
+
+}
